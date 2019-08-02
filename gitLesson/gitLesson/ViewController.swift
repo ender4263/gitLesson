737699c7d22f7fa3dd18dfb10c3dbf1e77886e04
Michael Moore <michaelmoore@Michaels-MacBook-Pro-2.local>
2019-08-02 15:17:00 -0600

    First commit

---
+++ gitLesson/gitLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitLesson
+//
+//  Created by Michael Moore on 8/2/19.
+//  Copyright © 2019 Michael Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
